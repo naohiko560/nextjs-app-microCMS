c61a8222fea1da4487d79613f9c1d2e36b90caec
naohiko560 <private.mail.nao@gmail.com>
2023-11-03 17:56:53 +0900

    コンテンツ取得用クライアントの作成

---
+++ libs/microcms.ts
@@ -0,0 +1,52 @@
+import { createClient } from 'microcms-js-sdk';
+import type { MicroCMSQueries, MicroCMSImage, MicroCMSDate } from 'microcms-js-sdk';
+import { resolve } from 'path';
+
+// ブログの型定義
+export type Blog = {
+  id: string;
+  title: string;
+  content: string;
+  eyecatch?: MicroCMSImage;
+} & MicroCMSDate;
+
+if (!process.env.MICROCMS_API_DOMAIN) {
+  throw new Error('MICROCMS_API_DOMAIN は必須です');
+}
+
+if (!process.env.MICROCMS_API_KEY) {
+  throw new Error('MICROCMS_API_KEY は必須です');
+}
+
+// API取得用のクライアントを作成
+export const client = createClient({
+  serviceDomain: process.env.MICROCMS_API_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY,
+});
+
+// ブログ一覧を取得
+export const getList = async (queries?: MicroCMSQueries) => {
+  const listData = await client.getList<Blog>({
+    endpoint: 'blogs',
+    queries,
+  });
+
+  // データの取得が目視しやすいよう明示的に遅延効果を追加
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  return listData;
+};
+
+// ブログ詳細を取得
+export const getDetail = async (contentId: string, queries?: MicroCMSQueries) => {
+  const detailData = await client.getListDetail<Blog>({
+    endpoint: 'blogs',
+    contentId,
+    queries,
+  });
+
+  // データの取得が目視しやすいよう明示的に遅延効果を追加
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  return detailData;
+};
