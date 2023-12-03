e3fb2db21e04ac4a6d171bad303aa96cdb0d17b9
naohiko560 <private.mail.nao@gmail.com>
2023-12-03 10:41:24 +0900

    カテゴリ一覧ページ仮設置

---
+++ src/app/category/[slug]/page.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import { getList } from '../../../libs/microcms';
+
+export default async function Home() {
+  const { contents } = await getList();
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      {contents.map((data) => {
+        const updatedAt = data.updatedAt;
+        const update = updatedAt.substring(0, 10);
+
+
+        return (
+          <Link href={`posts/${data.id}/`} key={data.id} className="md:hover:underline">
+            <div className="border border-violet-200 p-4 rounded-md shadow-md bg-white">
+              <h2 className="font-bold text-violet-600 mt-1">{data.title}</h2>
+              <p className="text-sm text-slate-400 mt-1">{update}</p>
+              <p className="text-sm text-slate-400 mt-1">{data.category.name}</p>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
