8d56bf317cd744f01f58e517ae962fde4a6a46a0
naohiko560 <private.mail.nao@gmail.com>
2023-12-09 21:38:47 +0900

    ハンバーガーメニュー：Link→a

--- src/components/Burger.tsx
+++ src/components/Burger.tsx
@@ -3,51 +3,43 @@
 import '../app/styles/burger.css';
 import '../app/styles/globals.css';
 import { slide as Menu } from 'react-burger-menu';
-import React, { useState } from 'react';
-import Link from 'next/dist/client/link';
+import React from 'react';
 import Image from 'next/image';
 
 const Burger = () => {
-  const handleOnClose = () => {
-    handleOnClose();
-  };
-
   return (
     <div className="md:hidden">
       <Menu
         right
         customCrossIcon={<Image src="/images/common/cross.png" alt="✕" width="36" height="40" />}
-        onClose={handleOnClose}
       >
-        <Link id="home" className="" href="/" onClick={handleOnClose}>
+        <a id="home" className="" href="/">
           トップ
-        </Link>
-        <Link id="profile" className="" href="/profile" onClick={handleOnClose}>
+        </a>
+        <a id="profile" className="" href="/profile">
           プロフィール
-        </Link>
-        <Link id="contact" className="" href="/contact" onClick={handleOnClose}>
+        </a>
+        <a id="contact" className="" href="/contact">
           お問い合わせ
-        </Link>
-        <Link
+        </a>
+        <a
           id="x"
           className=""
           href="https://twitter.com/naohiko_blog"
           target="_blank"
           rel="noopener nofollow"
-          onClick={handleOnClose}
         >
           X
-        </Link>
-        <Link
+        </a>
+        <a
           id="github"
           className=""
           href="https://github.com/naohiko560"
           target="_blank"
           rel="noopener nofollow"
-          onClick={handleOnClose}
         >
           GitHub
-        </Link>
+        </a>
       </Menu>
     </div>
   );
