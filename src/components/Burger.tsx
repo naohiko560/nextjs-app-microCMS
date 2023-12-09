687e119e3f38f153f9231a58c989d4cdacd029cf
naohiko560 <private.mail.nao@gmail.com>
2023-12-10 00:03:09 +0900

    Link でも react-burger-menu を使用可能にする処理実装

--- src/components/Burger.tsx
+++ src/components/Burger.tsx
@@ -3,43 +3,61 @@
 import '../app/styles/burger.css';
 import '../app/styles/globals.css';
 import { slide as Menu } from 'react-burger-menu';
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Burger = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // 現在のisOpenと逆のブール値になる
+  const handleIsOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  // isOpenをfalseにする
+  const closeBurger = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div className="md:hidden">
       <Menu
         right
         customCrossIcon={<Image src="/images/common/cross.png" alt="✕" width="36" height="40" />}
+        isOpen={isOpen} // ハンバーガーメニューの開閉管理
+        onOpen={handleIsOpen} // ハンバーガーメニューが開いた場合に呼び出される
+        onClose={handleIsOpen} // ハンバーガーメニューが閉じた場合に呼び出される
       >
-        <a id="home" className="" href="/">
+        <Link id="home" className="" href="/" onClick={closeBurger}>
           トップ
-        </a>
-        <a id="profile" className="" href="/profile">
+        </Link>
+        <Link id="profile" className="" href="/profile" onClick={closeBurger}>
           プロフィール
-        </a>
-        <a id="contact" className="" href="/contact">
+        </Link>
+        <Link id="contact" className="" href="/contact" onClick={closeBurger}>
           お問い合わせ
-        </a>
-        <a
+        </Link>
+        <Link
           id="x"
           className=""
           href="https://twitter.com/naohiko_blog"
           target="_blank"
           rel="noopener nofollow"
+          onClick={closeBurger}
         >
           X
-        </a>
-        <a
+        </Link>
+        <Link
           id="github"
           className=""
           href="https://github.com/naohiko560"
           target="_blank"
           rel="noopener nofollow"
+          onClick={closeBurger}
         >
           GitHub
-        </a>
+        </Link>
       </Menu>
     </div>
   );
