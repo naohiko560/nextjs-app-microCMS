97239ee96876c2d05d5ce8d07e28464449253452
naohiko560 <private.mail.nao@gmail.com>
2023-12-10 23:12:54 +0900

    問い合わせフォームのスタイル実装

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,21 +3,52 @@ import '../styles/globals.css';
 // SSGform実装
 const ContactPage = () => {
   return (
-    <div>
+    <div className="max-w-md mx-auto mt-4 bg-white p-6 rounded shadow-md">
+      <h1 className="text-xl font-bold mb-4">お問い合わせフォーム</h1>
       <form action={process.env.SSG_FORM} method="post">
-        <div>
-          <label htmlFor="name">お名前</label>
-          <input id="name" type="text" name="お名前" className="bg-blue-400" />
+        <div className="mb-4">
+          <label htmlFor="name" className="block font-medium text-gray-600">
+            お名前<span className="text-red-600 ml-1 font-bold">※必須</span>
+          </label>
+          <input
+            id="name"
+            type="text"
+            name="お名前"
+            className="mt-1 p-2 w-full border rounded-md"
+            required
+          />
         </div>
-        <div>
-          <label htmlFor="email">メールアドレス</label>
-          <input id="email" type="email" name="メールアドレス" className="bg-blue-400" />
+        <div className="mb-4">
+          <label htmlFor="email" className="block font-medium text-gray-600">
+            メールアドレス<span className="text-red-600 ml-1 font-bold">※必須</span>
+          </label>
+          <input
+            id="email"
+            type="email"
+            name="メールアドレス"
+            className="mt-1 p-2 w-full border rounded-md"
+            required
+          />
         </div>
-        <div>
-          <label htmlFor="textarea">お問い合わせ内容</label>
-          <textarea id="textarea" name="お問い合わせ内容" className="bg-blue-400"></textarea>
+        <div className="mb-4">
+          <label htmlFor="textarea" className="block font-medium text-gray-600">
+            お問い合わせ内容<span className="text-red-600 ml-1 font-bold">※必須</span>
+          </label>
+          <textarea
+            id="textarea"
+            name="お問い合わせ内容"
+            className="mt-1 p-2 w-full border rounded-md"
+            required
+          ></textarea>
         </div>
-        <button type="submit">送信する</button>
+        <div className="flex justify-center">
+          <button
+            type="submit"
+            className="px-8 py-2 rounded text-white bg-blue-600 hover:bg-blue-700"
+          >
+            送信する
+          </button>
+        </div>
       </form>
     </div>
   );
