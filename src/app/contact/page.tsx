da0494ba8c7bb784345b3d194c4df32aa6827a82
naohiko560 <private.mail.nao@gmail.com>
2023-12-10 22:08:45 +0900

    問い合わせスタイル少し修正

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,9 +5,9 @@ const ContactPage = () => {
   return (
     <div>
       <form action={process.env.SSG_FORM} method="post">
-        <input type="text" name="お名前" required />
-        <input type="email" name="メールアドレス" required />
-        <textarea name="お問い合わせ内容" required></textarea>
+        <input type="text" name="お名前" required className="border-blue-400 " placeholder='お名前' />
+        <input type="email" name="メールアドレス" required className="border-blue-400" placeholder='メールアドレス' />
+        <textarea name="お問い合わせ内容" required className="border-blue-400" placeholder='お問い合わせ内容'></textarea>
         <button type="submit">送信する</button>
       </form>
     </div>
