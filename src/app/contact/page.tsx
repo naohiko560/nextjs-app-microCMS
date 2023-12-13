17047c83077642ef5505d80e471cfe5e7f5e18f2
naohiko560 <private.mail.nao@gmail.com>
2023-12-13 20:38:18 +0900

    フォームにwana実装

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,6 +6,9 @@ const ContactPage = () => {
     <div className="max-w-md mx-auto mt-4 bg-white p-6 rounded shadow-md">
       <h1 className="text-xl font-bold mb-4">お問い合わせフォーム</h1>
       <form action={process.env.SSG_FORM} method="post">
+        <div className="hidden">
+          <input type="text" name="wana" />
+        </div>
         <div className="mb-4">
           <label htmlFor="name" className="block font-medium text-gray-600">
             お名前<span className="text-red-600 ml-1 font-bold">※必須</span>
