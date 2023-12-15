2a955845419e284f53509b080c3b362ffce987a6
naohiko560 <private.mail.nao@gmail.com>
2023-12-16 08:34:32 +0900

    Revert "トップのテキストカラー濃くする"
    This reverts commit baafed17a7f97edb872702185fd47f69138bbda5.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,8 +19,8 @@ export default async function Home() {
           >
             <div className="border border-violet-200 p-4 rounded-md shadow-md bg-white">
               <h2 className="font-bold text-violet-600 mt-1">{data.title}</h2>
-              <p className="text-sm text-slate-500 mt-1">{update}</p>
-              <p className="text-sm text-slate-500 mt-1">{data.category.name}</p>
+              <p className="text-sm text-slate-400 mt-1">{update}</p>
+              <p className="text-sm text-slate-400 mt-1">{data.category.name}</p>
             </div>
           </Link>
         );
