8d2050b6ef414253505e9b1561c092cb07affee8
naohiko560 <private.mail.nao@gmail.com>
2023-12-17 14:24:22 +0900

    カテゴリーページ削除

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -1,62 +1,77 @@
-import Link from 'next/link';
-import { getList, getCategories, getDetail } from '@/src/libs/microcms';
-import type { Metadata } from 'next';
+// import Link from 'next/link';
+// import { getList, getCategories, client, getDetail } from '@/src/libs/microcms';
+// import type { Metadata } from 'next';
 
-export default async function CategoryId() {
-  const { contents } = await getList();
+// export default async function CategoryId() {
+//   const { contents } = await getList();
 
-  // カテゴリーに紐付いたコンテンツがない場合に表示
-  if (contents.length === 0) {
-    return <div>ブログコンテンツがありません</div>;
-  }
+//   // カテゴリーに紐付いたコンテンツがない場合に表示
+//   // if (category.length === 0) {
+//   //   return <div>ブログコンテンツがありません</div>;
+//   // }
 
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-      {contents.map((data) => {
-        const updatedAt = data.updatedAt;
-        const update = updatedAt.substring(0, 10);
+//   return (
+//     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+//       {contents.map((data: any) => {
+//         const updatedAt = data.updatedAt;
+//         const update = updatedAt.substring(0, 10);
 
-        return (
-          <Link href={`posts/${data.id}/`} key={data.id} className="md:hover:underline">
-            <div className="border border-violet-200 p-4 rounded-md shadow-md bg-white">
-              <h2 className="font-bold text-violet-600 mt-1">{data.title}</h2>
-              <p className="text-sm text-slate-500 mt-1">{update}</p>
-              <p className="text-sm text-slate-500 mt-1">{data.category.name}</p>
-            </div>
-          </Link>
-        );
-      })}
-    </div>
-  );
-}
+//         return (
+//           <Link href={`../../posts/${data.id}/`} key={data.id} className="md:hover:underline">
+//             <div className="border border-violet-200 p-4 rounded-md shadow-md bg-white">
+//               <h2 className="font-bold text-violet-600 mt-1">{data.title}</h2>
+//               <p className="text-sm text-slate-500 mt-1">{update}</p>
+//               <p className="text-sm text-slate-500 mt-1">{data.category.name}</p>
+//             </div>
+//           </Link>
+//         );
+//       })}
+//     </div>
+//   );
+// }
 
-// 静的生成するパスを指定
-export async function generateStaticParams() {
-  const { contents } = await getCategories();
+// // 静的生成するパスを指定
+// export async function generateStaticParams() {
+//   const { contents } = await getCategories();
 
-  const paths = contents.map((data) => {
-    return {
-      slug: data.id,
-    };
-  });
+//   const paths = contents.map((data) => {
+//     return {
+//       slug: data.id,
+//     };
+//   });
 
-  return [...paths];
-}
+//   return [...paths];
+// }
 
+// // export async function getCategory({
+// //   params: { slug },
+// // }: {
+// //   params: { slug: string };
+// // }): Promise<Metadata> {
+// //   const data = await getDetail(slug);
+// //   const category = data.category;
 
-// export async function generateMetadata({
-//   params: { slug },
-// }: {
-//   params: { slug: string };
-// }): Promise<Metadata> {
-//   const id = await getDetail(slug);
-//   const data = await getList({
+// //   const categoryBlogs = await client.get({
+// //     endpoint: 'blogs',
+// //     queries: { filters: `category[equals]${category}` },
+// //   });
+
+// //   return categoryBlogs;
+// // }
+
+// // データをテンプレートに受け渡す部分の処理を記述します
+// export async function Page({ params: { slug } }: { params: { slug: string } }) {
+//   const data = await getDetail(slug);
+//   const id = data.category;
+  
+//   const categoryId = await client.get({
+//     endpoint: 'blogs',
 //     queries: { filters: `category[equals]${id}` },
 //   });
 
 //   return {
-//     data: {
-//       blog: data.contents,
+//     props: {
+//       blog: categoryId.contents,
 //     },
 //   };
 // }
